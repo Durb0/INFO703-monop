1ce4955dd43781c6b47ed42b5f5bde7682610e3e
Durb0 <thomasnou@orange.fr>
2023-01-05 10:51:24 +0100

    Create caseAchetable.ts

---
+++ src/model/case/caseAchetable/caseAchetable.ts
@@ -0,0 +1,24 @@
+import { Case } from '../case';
+import type { Joueur } from '../../joueur';
+
+export abstract class CaseAchetable extends Case {
+    private prix:number;
+    private proprietaire:Joueur;
+
+    constructor(nom:string, prix:number) {
+        super(nom);
+        this.prix = prix;
+    }
+
+    public getPrix():number{
+        return this.prix;
+    }
+
+    public getProprietaire():Joueur{
+        return this.proprietaire;
+    }
+
+    public setProprietaire(proprietaire:Joueur):void{
+        this.proprietaire = proprietaire;
+    }
+}
